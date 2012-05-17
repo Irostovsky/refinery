22177a34e20034353ba1686305723608f00b541a
Ivan Rostovsky <ir@anahoret.com>
2012-05-17 13:33:26 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = 'b1800918e937a6dc6832f0822740d195ab5cce4f034772006447ce7647b0f29a929338717e666786dbdd38b1cacc4fa9ccac60ff0fbe2cb548b7796d0d52ee3d'
